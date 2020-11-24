6a4c034b80a710f5323c4f8db040fbbe708e26ec
aflueckiger <alex.flueckiger@gmail.com>
2020-11-24 17:49:30 +0100

    refactor and document code

--- lib/language_identification.py
+++ lib/language_identification.py
@@ -5,6 +5,7 @@ import logging
 import re
 import sys
 from collections import Counter
+from typing import Dict, List
 
 import fasttext
 import jsonlines
@@ -16,39 +17,97 @@ from smart_open import open
 log = logging.getLogger(__name__)
 
 
-def average_distribution(listoflist):
-    """
-    Return dictionary of averaged language:probability key-value pairs
-
-    """
-    total = len(listoflist)
-    counter = Counter()
-    for l in listoflist:
-        for r in l:
-            counter[r.lang] += r.prob
-    for k in counter:
-        counter[k] = counter[k] / total
-    result = [{"lang": k, "prob": round(counter[k], 2)} for k in counter]
-    log.debug(f"DEBUG-LANGDETECT-DIVERSITY {len(listoflist)} {listoflist}")
-    return result
-
-
-def alphabetical_ratio(text):
-    """Return the percentage of alphabetic characters
+def alphabetical_ratio(text: str) -> float:
+    """Return the percentage of alphabetic characters of a text
 
     All digits, punctuation symbols, layout characters, are removed
+
+    :param str text: Any text.
+    :return: Ratio of alphabetic characters wrt to total lenght of text.
+    :rtype: float
+
     """
+
     len_text = len(text)
     if len_text == 0:
         return None
     filtered = re.sub(r'[\W_\d]+', '', text)
-    # log.debug(f"FILTERED-TEXT {filtered}")
+
     return len(filtered) / len_text
 
 
-class MainApplication(object):
+def average_distribution(listoflist: List[List]) -> List[Dict[str, float]]:
+    """Return dictionary of averaged probabilities per language.
 
-    def __init__(self, args):
+    :param List[List] listoflist: Results of multiple language identification.
+    :return: Dictionary with the averaged probabilities per language
+    :rtype: List[Dict[str, float]]
+
+    """
+
+    total = len(listoflist)
+    counter = Counter()
+    for row in listoflist:
+        for r in row:
+            counter[r.lang] += r.prob
+    for lang in counter:
+        counter[lang] = counter[lang] / total
+    result = [{"lang": lang, "prob": round(counter[lang], 2)} for lang in counter]
+
+    log.debug(f"DEBUG-LANGDETECT-DIVERSITY Length: {len(listoflist)} Predictions: {listoflist}")
+
+    return result
+
+
+def avg_langdetect_lid(text: str, n: int, threshold: float = 0.95) -> List[Dict[str, float]]:
+    """Compute averaged lid score from n samples using Langdetect.
+
+    For efficiency, drawing stops if the top-most language has a higher probability than threshold
+
+    :param str text: Text to classify.
+    :param int n: Number of samples.
+    :param float threshold: Threshold for early-stopping of sampling.
+    :return: Dictionary with the averaged probabilities per language
+    :rtype: List[Dict[str, float]]
+
+    """
+
+    results = []
+    for i in range(n):
+        result = langdetect.detect_langs(text)
+        results.append(result)
+        if result[0].prob > threshold:
+            break
+
+    return average_distribution(results)
+
+
+def fasttext_lid(text: str, ft_model) -> List[Dict[str, float]]:
+    """
+    Return results of a fasttext model.
+
+    The only normalization is mapping digits to 0. The internal function predict of fasttext returns a pair
+    of tuples
+
+    In [16]: m.predict(''' l'eût cru, le rêve de M. Mitterand, c'est d'e''',k=3)
+    Out[16]: (('__label__fr', '__label__lb', '__label__de'),
+             array([9.99996185e-01, 2.38023513e-05, 1.00000034e-05]))
+    """
+
+    # ignore digits
+    text = re.sub(r'\d+', '', text)
+
+    labels, probs = ft_model.predict(text, k=3, threshold=0.005)
+    result = [{"lang": lang.replace('__label__', ''), "prob": float(min(1, round(probs[i], 2)))}
+              for (i, lang) in
+              enumerate(labels)]
+
+    return result
+
+
+class LanguageInfer():
+
+    def __init__(self, args: Dict) -> None:
         self.args = args
         self.detectors = ["langdetect", "langid"]  # default LID detectors
         self.jsonlog = {}
@@ -60,82 +119,48 @@ class MainApplication(object):
         self.language_identification()
         self.output()
 
-    def language_identification(self):
+    def language_identification(self) -> None:
+        """Run multiple language identifications with the models provided and update results
+
+        :return: None.
+        :rtype: None
+
         """
-        Run language identification with available models and update results
 
-
-        """
-
-        # langid lid classifier
+        # initialize with langid lid classifier
         langid_lid = LanguageIdentifier.from_modelstring(model, norm_probs=True)
-
         # we no longer restrict it to certain languages
         # langid_lid.set_languages(['de', 'fr', 'en', 'lb'])
 
-        # langdetect lid classifier using n samples from a text
-
-        def avg_langdetect_lid(text, n, threshold=0.95):
-            """
-            Compute averaged lid from at most n draws.
-
-            For efficiency, drawing stops if the top-most language has a higher probability than threshold
-            """
-            results = []
-            for i in range(n):
-                result = langdetect.detect_langs(text)
-                results.append(result)
-                if result[0].prob > threshold:
-                    break
-            return average_distribution(results)
-
-        # fasttext lid
+        # load provided FastText models
         impresso_ft_model = wp_ft_model = None
+
         if self.args.impresso_ft is not None:
             impresso_ft_model = fasttext.load_model(self.args.impresso_ft)
         if self.args.wp_ft is not None:
             wp_ft_model = fasttext.load_model(self.args.wp_ft)
 
-        def fasttext_lid(text, themodel):
-            """
-            Return results of a fasttext model
-
-            The only normalization is mapping digits to 0. The internal function predict of fasttext returns a pair
-            of tuples
-
-            In [16]: m.predict(''' l'eût cru, le rêve de M. Mitterand, c'est d'e''',k=3)
-            Out[16]: (('__label__fr', '__label__lb', '__label__de'),
-                     array([9.99996185e-01, 2.38023513e-05, 1.00000034e-05]))
-            """
-            if themodel is None:
-                return None
-            # ignore digits
-            text = re.sub(r'\d+', '', text)
-            labels, probs = themodel.predict(text, k=3, threshold=0.005)
-            result = [{"lang": l.replace('__label__', ''), "prob": float(min(1, round(probs[i], 2)))}
-                      for (i, l) in
-                      enumerate(labels)]
-            return result
-
-        # apply all models
+        # iterate over content items and apply all LID models
         for j in self.next_contentitem():
             log.info(f"WORKING ON {j['id']}")
             jinfo = {}
+
             try:
                 # initialize information
                 jinfo.update(
                     {
                         "tp": j["tp"],
-                        "cid": j["id"],
-                        "len": len(j["ft"]) if "ft" in j and type(j["ft"]) == str else 0,
+                        "id": j["id"],
+                        "len": len(j["ft"]) if "ft" in j and isinstance(j["ft"], str) else 0,
                         "orig_lg": j["lg"] if "lg" in j else None
                     }
                 )
 
-                if "ft" in j and type(j["ft"]) == str and len(j["ft"].strip()) >= self.args.minimal_text_length:
+                # perform lid if text of content item is available and has a minimal length
+                if "ft" in j and isinstance(j["ft"], str) and len(j["ft"].strip()) >= self.args.minimal_text_length:
                     jinfo["alphabetical_ratio"] = round(alphabetical_ratio(j["ft"]), 2)
 
-                    # langdetect
+                    # predict with langdetect
                     try:
                         langdetect_result = avg_langdetect_lid(j["ft"], 3)
                     except LangDetectException:
@@ -143,14 +168,13 @@ class MainApplication(object):
                         langdetect_result = None
                     jinfo["langdetect"] = langdetect_result
 
-                    # langid
+                    # predict with langid
                     try:
                         lang_orig, lang_prob_orig = langid_lid.classify(j["ft"])
                         jinfo["langid"] = [{"lang": lang_orig, "prob": round(lang_prob_orig, 2)}]
                     except:
                         log.error(f"LANGID-ERROR-WITH {sys.exc_info()[0]}")
                         jinfo["langid"] = None
-                        print(f'PROBLEM WITH {jinfo} {j["ft"]}', file=sys.stderr)
 
                     # fasttext with our own de/fr/lb model
                     if impresso_ft_model is not None:
@@ -161,6 +185,8 @@ class MainApplication(object):
                             log.error(f"IMPRESSO-FT-ERROR-WITH {sys.exc_info()[0]}")
                     else:
                         jinfo["impresso_ft"] = None
+
+                    # fasttext with public wikipedia model
                     if wp_ft_model is not None:
                         try:
                             jinfo["wp_ft"] = fasttext_lid(j["ft"], wp_ft_model)
@@ -179,15 +205,15 @@ class MainApplication(object):
         """
         Output json
         """
-        with open(self.args.output_file, mode="w", encoding="utf-8") as of:
-            writer = jsonlines.Writer(of)
+        with open(self.args.output_file, mode="w", encoding="utf-8") as f_out:
+            writer = jsonlines.Writer(f_out)
             writer.write_all(self.results)
 
     def next_contentitem(self):
         """"
         Yield each contentitem
         """
-        print(self.args.input_file, file=sys.stderr)
+
         with open(self.args.input_file, encoding="utf-8") as reader:
             json_reader = jsonlines.Reader(reader)
             for jdata in json_reader:
@@ -222,7 +248,8 @@ if __name__ == '__main__':
 
     log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
                   logging.INFO, logging.DEBUG]
+
     logging.basicConfig(level=log_levels[args.verbose],
                         format='%(asctime)-15s %(levelname)s: %(message)s')
 
-    MainApplication(args).run()
+    LanguageInfer(args).run()
